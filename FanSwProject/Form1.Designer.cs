f74d6872440e88bb843af8f801228a26214aff8b
Gradus fan <gradus.fan@manaflex.com>
2024-05-23 13:37:05 +0800

    添加项目文件。

---
+++ FanSwProject/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace FanSwProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btn_hand = new System.Windows.Forms.Button();
+            this.btn_set = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.butt0n7 = new System.Windows.Forms.Button();
+           // this.buttonHome = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.groudbox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groudbox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(7, 128);
+            this.buttonLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(155, 65);
+            this.buttonLogin.TabIndex = 1;
+            this.buttonLogin.Text = "button2";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(7, 211);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(155, 65);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btn_hand
+            // 
+            this.btn_hand.Image = ((System.Drawing.Image)(resources.GetObject("btn_hand.Image")));
+            this.btn_hand.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_hand.Location = new System.Drawing.Point(7, 294);
+            this.btn_hand.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_hand.Name = "btn_hand";
+            this.btn_hand.Size = new System.Drawing.Size(155, 65);
+            this.btn_hand.TabIndex = 5;
+            this.btn_hand.Text = "    手动";
+            this.btn_hand.UseVisualStyleBackColor = true;
+            this.btn_hand.Click += new System.EventHandler(this.btn_hand_Click);
+            // 
+            // btn_set
+            // 
+            this.btn_set.Image = ((System.Drawing.Image)(resources.GetObject("btn_set.Image")));
+            this.btn_set.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_set.Location = new System.Drawing.Point(7, 656);
+            this.btn_set.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_set.Name = "btn_set";
+            this.btn_set.Size = new System.Drawing.Size(155, 65);
+            this.btn_set.TabIndex = 4;
+            this.btn_set.Text = "   设置";
+            this.btn_set.UseVisualStyleBackColor = true;
+            this.btn_set.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
+            this.button6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.Location = new System.Drawing.Point(7, 751);
+            this.button6.Margin = new System.Windows.Forms.Padding(4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(155, 65);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "    日志";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // butt0n7
+            // 
+            this.butt0n7.Image = ((System.Drawing.Image)(resources.GetObject("butt0n7.Image")));
+            this.butt0n7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.butt0n7.Location = new System.Drawing.Point(7, 851);
+            this.butt0n7.Margin = new System.Windows.Forms.Padding(4);
+            this.butt0n7.Name = "butt0n7";
+            this.butt0n7.Size = new System.Drawing.Size(155, 65);
+            this.butt0n7.TabIndex = 6;
+            this.butt0n7.Text = "   关闭";
+            this.butt0n7.UseVisualStyleBackColor = true;
+            this.butt0n7.Click += new System.EventHandler(this.butt0n7_Click);
+            // 
+            // buttonHome
+            // 
+            //this.buttonHome.Image = ((System.Drawing.Image)(resources.GetObject("buttonHome.Image")));
+            //this.buttonHome.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            //this.buttonHome.Location = new System.Drawing.Point(7, 42);
+            //this.buttonHome.Margin = new System.Windows.Forms.Padding(4);
+            //this.buttonHome.Name = "buttonHome";
+            //this.buttonHome.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            //this.buttonHome.Size = new System.Drawing.Size(155, 65);
+            //this.buttonHome.TabIndex = 0;
+            //this.buttonHome.Text = "    主页";
+            //this.buttonHome.UseVisualStyleBackColor = true;
+            //this.buttonHome.Click += new System.EventHandler(this.buttonHome);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 7);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(187, 56);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panelMain
+            // 
+            this.panelMain.Location = new System.Drawing.Point(0, 60);
+            this.panelMain.Margin = new System.Windows.Forms.Padding(4);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Padding = new System.Windows.Forms.Padding(0, 100, 0, 0);
+            this.panelMain.Size = new System.Drawing.Size(1714, 1047);
+            this.panelMain.TabIndex = 7;
+            // 
+            // groudbox1
+            // 
+            this.groudbox1.Controls.Add(this.butt0n7);
+           // this.groudbox1.Controls.Add(this.buttonHome);
+            this.groudbox1.Controls.Add(this.button2);
+            this.groudbox1.Controls.Add(this.buttonLogin);
+            this.groudbox1.Controls.Add(this.button6);
+            this.groudbox1.Controls.Add(this.btn_hand);
+            this.groudbox1.Controls.Add(this.button7);
+            this.groudbox1.Controls.Add(this.button3);
+            this.groudbox1.Controls.Add(this.btn_set);
+            this.groudbox1.Controls.Add(this.button1);
+            this.groudbox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.groudbox1.Location = new System.Drawing.Point(1721, 60);
+            this.groudbox1.Name = "groudbox1";
+            this.groudbox1.Size = new System.Drawing.Size(156, 1047);
+            this.groudbox1.TabIndex = 9;
+            this.groudbox1.TabStop = false;
+            this.groudbox1.Text = "菜单";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(7, 560);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 65);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(7, 376);
+            this.button7.Margin = new System.Windows.Forms.Padding(4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(155, 65);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 467);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 65);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button2";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1877, 1107);
+            this.Controls.Add(this.groudbox1);
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(0, 60, 0, 0);
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Style = MetroFramework.MetroColorStyle.Orange;
+            this.TransparencyKey = System.Drawing.Color.SkyBlue;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groudbox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        //private System.Windows.Forms.Button buttonHome;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btn_hand;
+        private System.Windows.Forms.Button btn_set;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button butt0n7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.GroupBox groudbox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ FanSwProject/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace FanSwProject
+{
+    public partial class Form1 : MetroForm
+    {
+
+
+        private Form2 form2 = new Form2();
+        private Form3 form3 = new Form3();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //主页
+        private void buttonHome(object sender, EventArgs e)
+        {
+
+        }
+   
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            openForm(new Form3());
+        }
+
+        private void butt0n7_Click(object sender, EventArgs e)
+        {
+            DialogResult result =MessageBox.Show("确定要退出软件吗?", "WARNING", MessageBoxButtons.OKCancel);
+            if (result == DialogResult.OK)
+                Application.Exit();
+            else
+                return;
+
+        }
+
+        private void  openForm(Form frm)
+        {
+            frm.TopLevel = false;
+            frm.TopMost = false;
+            this.panelMain.Controls.Clear();
+            this.panelMain.Controls.Add(frm);
+            frm.Show();
+        }
+        //手动按钮
+        private void btn_hand_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //private void InitializeComponent()
+        //{
+        //    this.SuspendLayout();
+        //    // 
+        //    // Form1
+        //    // 
+        //    this.ClientSize = new System.Drawing.Size(1392, 740);
+        //    this.Name = "Form1";
+        //    this.ResumeLayout(false);
+
+        //}
+    }
+}
